686e563abba01bcf75d566e52f968ccc0cb8c525
Sunny-2610 <sunnysinha9884@gmail.com>
2026-01-22 12:55:33 +0530

    learned basic about the frontend

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div className="bg-[#121212] h-screen flex items-center justify-center">
+            <h1 className="text-center text-white text-2xl">login</h1>
+        </div>
+    )
+}
